ae55b5f0fa9bceabfc459c0f5f7c60f9e9e8c0ad
John Borak <john.borak@gmail.com>
2020-07-10 13:43:35 -0400

    changed package structure to include only binary no directory structure.

--- pktriot.rb
+++ pktriot.rb
@@ -1,13 +1,13 @@
 class Packetriot < Formula
   desc "Packetriot.com client supports tunneling Internet request to local or private servers."
   homepage "https://packetriot.com"
-  url "https://pktriot-dl-bucket.sfo2.digitaloceanspaces.com/releases/macos/pktriot-0.9.16.macos.tar.gz"
-  sha256 "0d046c7738bb85d74a0b46211e6cd25770de7adeda5af75e60c86b91bddf97e3"
+  url "https://pktriot-dl-bucket.sfo2.digitaloceanspaces.com/releases/macos/brew/pktriot-0.9.16.tar.gz"
+  sha256 "8fa95deb65119d98abbab00707920f846477d86747ccb3368d2585d26e5eb5fe"
 
   bottle :unneeded
 
   def install
-    bin.install "pktriot-0.9.16/pktriot"
+    bin.install "pktriot"
   end
 
   test do
