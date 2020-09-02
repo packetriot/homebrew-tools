afa4e8188782afab632dea185025c3f4b120598f
John Borak <john.borak@gmail.com>
2020-09-02 05:58:01 -0400

    build issue, updating hash

--- pktriot.rb
+++ pktriot.rb
@@ -2,7 +2,7 @@ class Pktriot < Formula
   desc "Packetriot.com client supports tunneling Internet request to local or private servers."
   homepage "https://packetriot.com"
   url "https://pktriot-dl-bucket.sfo2.digitaloceanspaces.com/releases/macos/brew/pktriot-0.9.19.tar.gz"
-  sha256 "c56d53c38d2f8a7e7b2e07e8e26d0816015751212a9f5174cc15651e126aa06c"
+  sha256 "235b09b759937186221d191f3414426136c3ff8377b0c75cea17d0b735b8cefc"
 
   bottle :unneeded
 
