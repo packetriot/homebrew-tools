6fe6a3d7409532ae4d889eb1460ae31bf3180faa
packetriot <50630506+packetriot@users.noreply.github.com>
2020-09-07 14:32:00 -0400

    Update pktriot.rb
    Updating hash

--- pktriot.rb
+++ pktriot.rb
@@ -2,7 +2,7 @@ class Pktriot < Formula
   desc "Packetriot.com client supports tunneling Internet request to local or private servers."
   homepage "https://packetriot.com"
   url "https://pktriot-dl-bucket.sfo2.digitaloceanspaces.com/releases/macos/brew/pktriot-0.9.19.tar.gz"
-  sha256 "235b09b759937186221d191f3414426136c3ff8377b0c75cea17d0b735b8cefc"
+  sha256 "3543092b677405768d7df91d3183dbca04c9d1128876481af45673943e414744"
 
   bottle :unneeded
 
