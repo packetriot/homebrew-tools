1e1eeca900d6ac49f7f6a2e5168f7723f6af7beb
John Borak <john.borak@gmail.com>
2020-09-13 16:39:25 -0400

    fixing the URL manually, script produced this value incorrectly fixed that too.

--- pktriot.rb
+++ pktriot.rb
@@ -2,7 +2,7 @@ class Pktriot < Formula
   desc "Packetriot.com client supports tunneling Internet requests to local or private servers."
   homepage "https://packetriot.com"
 
-  url "https://download.packetriot/macos/brew/pktriot-0.9.19.tar.gz"
+  url "https://download.packetriot.com/macos/brew/pktriot-0.9.19.tar.gz"
   sha256 "41b8213ecd24c7ee089633381e72a915169fb6bc86219938f64f59dc036f12cd"
 
   bottle :unneeded
