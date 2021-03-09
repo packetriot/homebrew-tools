e088c6048d8fec6735f269b6e8e2c92cdcb76bec
John Borak <john.borak@gmail.com>
2021-03-09 12:41:14 -0500

    more installation script tweaks for v1.1.1

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.1.tar.gz"
-  sha256 "0b1944d9870e601ef18b14545c4089e07456b51051122641b564515bee44f134"
+  sha256 "80f5a1804c40ffdaaf7f2345bc4e5f9c591998309188413006a44b6488e83bdb"
 
   bottle :unneeded
 
