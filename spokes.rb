100641a5252047c3051d83285532baa2597730df
John Borak <john.borak@gmail.com>
2021-03-08 20:42:01 -0500

    fixed bug in generating the sha256 hash line

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.1.tar.gz"
-  sha256 "4068b5df4e46cdbed5091ec1aa2e5d5540444695e9edbc99f6f3d0949d656ca0  build/spokes/pkg/brew/spokes-1.1.1.tar.gz"
+  sha256 "c45c04d36180b167e5876b12b555bc6fd559b077b079cd0ede17edd5141ca3c1"
 
   bottle :unneeded
 
