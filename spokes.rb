3f26090f23f43dcb637d5dc060c88dbd32136f9b
John Borak <john.borak@gmail.com>
2021-03-09 12:29:18 -0500

    updated v1.1.1 build, putting files in share/spokes instead of var/spokes.

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.1.tar.gz"
-  sha256 "c45c04d36180b167e5876b12b555bc6fd559b077b079cd0ede17edd5141ca3c1"
+  sha256 "0b1944d9870e601ef18b14545c4089e07456b51051122641b564515bee44f134"
 
   bottle :unneeded
 
