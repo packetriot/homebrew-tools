4595df61988504d04fec3c06cfd8085bd121004f
John Borak <john.borak@gmail.com>
2021-03-09 17:06:20 -0500

    built on mac to solve cgo issues

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.1.tar.gz"
-  sha256 "b679a85dcb29230e069cb07ecc80f4cfc794fb9be1ca1adc40e41639e4c76880"
+  sha256 "45e9794fedf5daac8b7a4a26fa7b770af887fd1ed3cdff770c0fa5e97007e5c8"
 
   bottle :unneeded
 
