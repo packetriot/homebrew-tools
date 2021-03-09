a9ce8dce6b050f4754ef2b193d72881061f19c3b
packetriot <50630506+packetriot@users.noreply.github.com>
2021-03-08 19:31:54 -0500

    Update spokes.rb
    manual fix

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.1.tar.gz"
-  sha256 "4068b5df4e46cdbed5091ec1aa2e5d5540444695e9edbc99f6f3d0949d656ca0  build/spokes/pkg/brew/spokes-1.1.1.tar.gz"
+  sha256 "4068b5df4e46cdbed5091ec1aa2e5d5540444695e9edbc99f6f3d0949d656ca0"
 
   bottle :unneeded
 
