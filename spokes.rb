7afde06eb72dc85a31d8903599d395c72d4b83dd
John Borak <john.borak@gmail.com>
2021-04-11 14:23:53 -0400

    updating hash

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.2.tar.gz"
-  sha256 "9ead62ddf87051fedec3973a7b641971cde20acf07027c6f2af0926afeeac147"
+  sha256 "85795780f852b049dd1104d4e94bed153f6b7026d37d3b94b83afd1e05bde2bc"
 
   bottle :unneeded
 
