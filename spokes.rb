c7f19b3d55d86fac880224faad41daefda1ca009
John Borak <john.borak@gmail.com>
2021-05-05 21:05:11 -0400

    fix for brew install prefix, hopefully

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.2.tar.gz"
-  sha256 "85795780f852b049dd1104d4e94bed153f6b7026d37d3b94b83afd1e05bde2bc"
+  sha256 "f1787de3eb7dc2ae323e1b1808d3e99f122b77208bb3a732f03398f75c616a4d"
 
   bottle :unneeded
 
