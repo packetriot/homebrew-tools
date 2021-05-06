8901d3bbbf523bba6ccd1c1ac9f516bef65da20b
John Borak <john.borak@gmail.com>
2021-05-05 21:11:05 -0400

    updated v1.1.2 package

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.1.2.tar.gz"
-  sha256 "46d5a707321cde4e8dc45f304a5f32e847a4c2bf1d742d84da510629b0dbe70b"
+  sha256 "2fcee06bc0c373ce9aa4d4c5c4bb0e423f5882b37d4f9d9c69fb31842f073562"
 
   bottle :unneeded
 
