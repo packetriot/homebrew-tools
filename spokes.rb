7f60c46d4486cb7b6c9cfdfed99b51f7b535623f
John Borak <john.borak@gmail.com>
2021-07-05 12:59:16 -0400

    v1.2.3 update

--- spokes.rb
+++ spokes.rb
@@ -2,8 +2,8 @@ class Spokes < Formula
   desc "Spokes is an HTTP and TCP tunneling server."
   homepage "https://packetriot.com/enterprise"
 
-  url "https://download.packetriot.com/macos/brew/spokes-1.2.2.tar.gz"
-  sha256 "0f22715e8296012ca06409777dd3df20225fd58a837917af08a402fcd26a3384"
+  url "https://download.packetriot.com/macos/brew/spokes-1.2.3.tar.gz"
+  sha256 "0325889ea15b3391d954e783c3df2860ae66be2e6c3afdf2342448bb1d7c2f9a"
 
   bottle :unneeded
 
