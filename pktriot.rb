36cd3bbd2e25922677eb3b1b337b6fd3368316a6
John Borak <john.borak@gmail.com>
2021-08-01 17:11:39 -0400

    updated for revision v0.11.0

--- pktriot.rb
+++ pktriot.rb
@@ -2,8 +2,8 @@ class Pktriot < Formula
   desc "Packetriot.com client supports tunneling Internet requests to local or private servers."
   homepage "https://packetriot.com"
 
-  url "https://download.packetriot.com/macos/brew/pktriot-0.10.10.tar.gz"
-  sha256 "cf8177716d623acf64761ade874905b627633874a02f56c7eb4cde3b8cb1b0ea"
+  url "https://download.packetriot.com/macos/brew/pktriot-0.11.0.tar.gz"
+  sha256 "888ac156b757a9a6a3be8215becc7b6ac683a4125d868cf204636efb5812eee2"
 
   bottle :unneeded
 
