d5f76f55fa03f38aa49f89bed5fc8910a26221b4
John Borak <john.borak@gmail.com>
2021-08-11 21:56:33 -0400

    updated brew install script to fix migration path

--- spokes.rb
+++ spokes.rb
@@ -3,7 +3,7 @@ class Spokes < Formula
   homepage "https://packetriot.com/enterprise"
 
   url "https://download.packetriot.com/macos/brew/spokes-1.3.0.tar.gz"
-  sha256 "66bfbe96777988e249cc81070201a7bf344606da3dada8ce2b6983ea99201f04"
+  sha256 "d926ba3ed836f5b6fcc7a5a987494d9ed3389d8ee52e7b48cc471a5fed771f07"
 
   bottle :unneeded
 
